6aa53b4e139b078e95429c8471b6edbed1fad235
reginareg <regina.bogde@gmail.com>
2022-04-02 18:05:50 +0300

    numbers

---
+++ js/02-numbers.js
@@ -0,0 +1,61 @@
+/*
+NUMBERS:
+- normalus:
+  - sveikieji
+  - desimtainiai
+- nenormalus:
+  -NaN (not-a-number)
+  - Infinity; -Infinity
+*/
+
+const sveikasis = 5;
+const desimtainis = 3.14;
+const neigiamasDesimtainis = -3.14;
+const neSkaicius = NaN;
+const begalybe = Infinity;
+
+const pirmas = 7;
+const antras = 5;
+
+const dalmuo =pirmas / antras;
+console.log(dalmuo);
+
+console.log(7 + 5);
+console.log(7 - 5);
+console.log(7 * 5);
+console.log(7 / 5);
+
+console.log(7 + '5');
+console.log(7 - '5');
+console.log(7 * '5');
+console.log(7 / '5');
+
+console.log('7' + '5');
+console.log('7' - '5');
+console.log('7' * '5');
+console.log('7' / '5');
+
+console.log('a' + 'b');
+console.log('a' - 'b');
+console.log('a' * 'b');
+console.log('a' / 'b');
+
+console.log(7 + true);
+console.log(7 - true);
+console.log(7 * true);
+console.log(7 / true);
+
+console.log(7 + false);
+console.log(7 - false);
+console.log(7 * false);
+console.log(7 / false);
+
+console.log(true + 5);
+console.log(true - 5);
+console.log(true * 5);
+console.log(true / 5);
+
+console.log(false + 5);
+console.log(false - 5);
+console.log(false * 5);
+console.log(false / 5);
