b1df08df6d6d565309fe06bebd17e5e3a0ffe48f
reginareg <regina.bogde@gmail.com>
2022-04-02 15:57:48 +0300

    examples

---
+++ js/variables.js
@@ -0,0 +1,43 @@
+/*
+
+KINTAMIEJI
+
+Iniciavimo budas:
+const - default inicijavimo budas
+let - antrinis budas inicijuoti, kai zinome, jog reiksme tures keistis
+var - pats senoiausias ir geriau nenaudoti jei rupi saugumas
+
+Duomenu tipas:
+- number
+- string (tekstas)
+- boolean (logine reiksme:true/false)
+- array (masyvas, sarasas, matrica*)
+
+*/
+const myLuckyNumberIs = 123;
+console.log('Lucky number:', myLuckyNumberIs);
+
+let pinigine = 0;
+console.log('Pinigine:',pinigine);
+
+pinigine = 5
+console.log('Pinigine:', pinigine);
+
+const vardas = 'Maryte'
+const  pasisveikinimas = 'Laba diena, '+ vardas +'.';
+console.log(pasisveikinimas);
+
+const arVedes = true;
+const arTuriVaiku = false;
+const arMeluojaDelVaiku = true;
+console.log('Ar meluoja?', arMeluojaDelVaiku);
+console.log('Ar vedes?', arVedes);
+console.log('Turi vaiku?', arTuriVaiku);
+
+console.log(2 + 2 * 2);
+console.log('2 + 2 * 2');
+
+const pazymiai = [10, 3, 6, 9, 5];
+console.log('Pazymiai:', pazymiai);
+
+
