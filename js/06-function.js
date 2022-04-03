df295ae0e1291eb1c0e2725f16c103fc19c8e7fa
reginareg <regina.bogde@gmail.com>
2022-04-03 20:34:56 +0300

    if

--- js/06-function.js
+++ js/06-function.js
@@ -28,3 +28,39 @@ const onosVidurkis = vidurkis(ona);
 console.log('Ona:', petroVidurkis);
 
 
+/*
+Funkcija gauna varda ir turi grazinti sakini:
+"Vardo pirma raide yra [V] ir vardas sudarytas is [5] raidziu."
+*/ 
+
+function vardoAnalize(name) {
+    return `Vardo "${name}" pirma raide yra ${name[0]} ir vardas sudarytas is ${name.length} raidziu.`;
+}
+
+const petroVardas = vardoAnalize('Petras');
+console.log(petroVardas);
+
+const marytesVardas = vardoAnalize('Maryte');
+console.log(marytesVardas);
+
+const onosVardas = vardoAnalize('Ona');
+console.log(onosVardas);
+
+/*
+Funkcija gauna du skaicius ir turi grazinti lygti su sprendimu, pvz.:
+2 + 2 = 4
+*/
+
+function suma(a, b) {
+    return `${a} + ${b} = ${a + b}`;
+}
+
+const s1 = suma(2, 3);
+console.log(s1);
+
+const s2 = suma(0.1, 0.2);
+console.log(s2);
+
+const s3 = suma(-88, -11);
+console.log(s3);
+
