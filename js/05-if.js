e314fcd03aead193f6db46093d93daed25633389
reginareg <regina.bogde@gmail.com>
2022-04-03 16:03:47 +0300

    if

---
+++ js/05-if.js
@@ -0,0 +1,21 @@
+/*
+IF
+
+Sablonai:
+- if () {}
+- if () {} else {} 
+
+Palyginimo operatoriai:
+- visi: >, <, <=, >=, ==, !=, ===
+- naudotini: >, <, <=, >=, ===
+- NEnaudotini: ==, !=
+*/
+
+const a = 7;
+const b = 55;
+
+if (a > b) {
+    console.log('A yra daugiau uz B');
+} else {
+     console.log('A nera daugiau uz B');  
+}
