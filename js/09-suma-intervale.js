e4afa0bd06ed6c73c4454bc979fd42ac144ab9cc
reginareg <regina.bogde@gmail.com>
2022-04-05 22:45:48 +0300

    suma intervale

---
+++ js/09-suma-intervale.js
@@ -0,0 +1,27 @@
+/*
+1.	Suskaičiuoti ką gausime susumavus skaičius intervale tarp (imtinai):
+a.	0 … 0
+b.	0 … 4
+c.	0 … 100
+d.	574 … 815
+e.	-50 … 50
+f.	-70 … 30
+*/
+function sumaIntervale(nuo, iki) {
+    let suma = 0;
+    for (let i = nuo; i <= iki; i++) {
+        suma += i;
+    }
+    return suma;
+}
+
+console.log(sumaIntervale(0, 0), '-->', 0);
+console.log(sumaIntervale(0, 1), '-->', 1);
+console.log(sumaIntervale(0, 2), '-->', 3);
+console.log(sumaIntervale(0, 3), '-->', 6);
+console.log(sumaIntervale(0, 4), '-->', 10);
+console.log(sumaIntervale(10, 14), '-->', 60);
+console.log(sumaIntervale(0, 100), '-->', 5050);
+console.log(sumaIntervale(574, 815), '-->', 168069);
+console.log(sumaIntervale(-50, 50), '-->', 0);
+console.log(sumaIntervale(-70, 30), '-->', -2020);
